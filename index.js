3d689872c63b57b1eeb219e7c9704bc484e54ade
ARC Sebastian <arcsbstn@gmail.com>
2021-09-01 09:11:47 +0800

    add basic layout

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
+const contentWrapperStyle = {
+  color: "#fff",
+  height: "100vh",
+  backgroundColor: "#333"
+}
+
+const rowWrapperSyle = {
+  height: "100%",
+  margin: "0"
+}
+
 class Root extends React.Component {
 
   render() {
     return (
-      <div>Hello world!</div>
+      <div id="content-wrapper"
+        className="container-fluid"
+        style={contentWrapperStyle}>
+        <div className="row align-items-center justify-content-center"
+          style={rowWrapperSyle}>
+          <div className="row w-75">
+            <div id="pads" className="col-md">A</div>
+            <div id="controls" className="col-md">B</div>
+          </div>
+        </div>
+
+      </div>
     )
   }
 }
